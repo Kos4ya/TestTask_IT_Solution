1f594553db3b805df9f9e5bc7d9fa89da2c734d0
Kos4ya <palenovkd@mail.ru>
2025-07-18 11:41:45 +0300

    Add: Urls path for quotes

--- project/quotes/urls.py
+++ project/quotes/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from .views import (RandomQuoteView, AddQuoteView,
-                                       AddSourceView, LikeDislikeView,
-                                       TopQuotesView, SourceListView)
+                   AddSourceView, LikeDislikeView,
+                   TopQuotesView, SourceListView,
+                    RegisterView)
 
 urlpatterns = [
     path('', RandomQuoteView.as_view(), name='random_quote'),
@@ -11,5 +12,10 @@ urlpatterns = [
     path('like-dislike/<int:quote_id>/<str:action>/',
          LikeDislikeView.as_view(), name='like_dislike'),
     path('top/', TopQuotesView.as_view(), name='top_quotes'),
-    path('sources/', SourceListView.as_view(), name='source_list')
+    path('sources/', SourceListView.as_view(), name='source_list'),
+    path('sources/<int:source_id>/', SourceListView.as_view(), name='source_detail'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/register/', RegisterView.as_view(), name='register'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
